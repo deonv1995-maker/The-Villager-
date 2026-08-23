4616689537c0424ce563c7e5b04317c2982a201e
deonv1995-maker <deonv1995@gmail.com>
2026-08-23 19:47:52 +0200

    Hotfix v0.7.3 remove broken opaque environment atlas

--- src/phaser-game.js
+++ src/phaser-game.js
@@ -124,7 +124,8 @@ class VillagerScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image('environment-atlas', assetUrl('environmentAtlas'));
+    this.load.image('tree', assetUrl('tree'));
+    this.load.image('rock', assetUrl('rock'));
     this.load.image('grass', assetUrl('grass'));
     this.load.spritesheet('player', assetUrl('playerSheet'), {
       frameWidth: release.playerAtlas.frameWidth,
@@ -136,19 +137,6 @@ class VillagerScene extends Phaser.Scene {
     });
   }
 
-  registerEnvironmentAtlasFrames() {
-    const texture = this.textures.get('environment-atlas');
-    if (!texture || texture.has('cottage')) return;
-
-    texture.add('cottage', 0, 0, 0, 240, 200);
-    texture.add('hall', 0, 240, 0, 240, 200);
-    texture.add('workshop', 0, 480, 0, 240, 200);
-    texture.add('tree', 0, 0, 200, 160, 208);
-    texture.add('rock', 0, 160, 200, 144, 104);
-    texture.add('well', 0, 304, 200, 96, 112);
-    texture.add('path', 0, 400, 200, 96, 96);
-  }
-
   create() {
     window.__THE_VILLAGER_SCENE__ = this;
 
@@ -156,7 +144,6 @@ class VillagerScene extends Phaser.Scene {
     this.cameras.main.setBounds(0, 0, GAME_CONFIG.world.width, GAME_CONFIG.world.height);
     this.physics.world.setBounds(0, 0, GAME_CONFIG.world.width, GAME_CONFIG.world.height);
 
-    this.registerEnvironmentAtlasFrames();
     this.worldArt = createWorldArt(this, GAME_CONFIG);
     this.village = createVillage(this);
     this.createResources();
@@ -177,22 +164,14 @@ class VillagerScene extends Phaser.Scene {
 
   createResources() {
     for (const node of this.resources) {
-      let sprite;
-      if (node.type === 'tree') {
-        sprite = this.add.image(node.x, node.y, 'environment-atlas', 'tree');
-        sprite.setDisplaySize(160, 208);
-        sprite.setOrigin(0.5, 0.90);
-      } else if (node.type === 'rock') {
-        sprite = this.add.image(node.x, node.y, 'environment-atlas', 'rock');
-        sprite.setDisplaySize(144, 104);
-        sprite.setOrigin(0.5, 0.82);
-      } else {
-        sprite = this.add.image(node.x, node.y, 'grass');
-        sprite.setDisplaySize(64, 76);
-        sprite.setOrigin(0.5, 0.88);
-      }
-
+      const sprite = this.add.image(node.x, node.y, node.type);
+      sprite.setOrigin(0.5, 0.88);
       sprite.setDepth(node.y);
+
+      if (node.type === 'tree') sprite.setDisplaySize(175, 252);
+      else if (node.type === 'rock') sprite.setDisplaySize(124, 100);
+      else sprite.setDisplaySize(64, 76);
+
       this.resourceSprites.set(node, sprite);
     }
   }
