8b37628da5f72f6dee0f815a100d41227b21f0b0
deonv1995-maker <deonv1995@gmail.com>
2026-08-25 15:12:05 +0200

    Expand KayKit forest species and nature families

--- src/systems/world/EnvironmentPopulation.js
+++ src/systems/world/EnvironmentPopulation.js
@@ -13,6 +13,7 @@ export class EnvironmentPopulation {
   const mat=(color,extra={})=>new T.MeshStandardMaterial({color,roughness:.82,metalness:0,flatShading:false,...extra});
   return {
    tree:mat(0xffffff,{vertexColors:true}),
+   bareTree:mat(0x755038),
    rock:mat(0x7d8681),
    bush:mat(0x4f8747),
    grass:mat(0x629b4d,{side:T.DoubleSide})
@@ -22,30 +23,27 @@ export class EnvironmentPopulation {
   return {
    tree:[
     {name:'natural',scale:[1,1,1]},
-    {name:'young',scale:[.82,.86,.82]},
-    {name:'mature',scale:[1.12,1.08,1.12]}
+    {name:'young',scale:[.84,.88,.84]},
+    {name:'mature',scale:[1.1,1.08,1.1]}
+   ],
+   bareTree:[
+    {name:'bare',scale:[1,1,1]},
+    {name:'weathered',scale:[.9,1.12,.9]}
    ],
    rock:[
-    {name:'boulder',scale:[1.35,1.05,1.25]},
-    {name:'flat',scale:[1.7,.55,1.35]},
-    {name:'tall',scale:[.82,1.5,.9]},
-    {name:'small',scale:[.62,.62,.7]},
-    {name:'wide',scale:[1.9,.8,1.05]},
-    {name:'upright',scale:[.7,1.9,.72]}
+    {name:'natural',scale:[1,1,1]},
+    {name:'small',scale:[.7,.7,.7]},
+    {name:'large',scale:[1.3,1.15,1.25]}
    ],
    bush:[
-    {name:'round',scale:[1,1,1]},
-    {name:'wide',scale:[1.65,.72,1.25]},
-    {name:'tall',scale:[.8,1.35,.82]},
-    {name:'small',scale:[.62,.62,.62]},
-    {name:'dense',scale:[1.28,1.08,1.22]}
+    {name:'natural',scale:[1,1,1]},
+    {name:'small',scale:[.75,.75,.75]},
+    {name:'full',scale:[1.22,1.1,1.18]}
    ],
    grass:[
-    {name:'short',scale:[1,.72,1]},
-    {name:'standard',scale:[1,1,1]},
-    {name:'tall',scale:[.85,1.42,.85]},
-    {name:'wide',scale:[1.45,.85,1.35]},
-    {name:'tiny',scale:[.55,.5,.55]}
+    {name:'short',scale:[1,.78,1]},
+    {name:'natural',scale:[1,1,1]},
+    {name:'tall',scale:[.9,1.25,.9]}
    ]
   };
  }
@@ -80,11 +78,22 @@ export class EnvironmentPopulation {
    this.loadObj('./assets/kaykit/forest/Tree_1_A_Color1.obj','tree'),
    this.loadObj('./assets/kaykit/forest/Tree_2_A_Color1.obj','tree'),
    this.loadObj('./assets/kaykit/forest/Tree_4_A_Color1.obj','tree'),
+   this.loadObj('./assets/kaykit/forest/Tree_Bare_1_A_Color1.obj','bareTree'),
    this.loadObj('./assets/kaykit/forest/Rock_1_A_Color1.obj','rock'),
+   this.loadObj('./assets/kaykit/forest/Rock_2_A_Color1.obj','rock'),
+   this.loadObj('./assets/kaykit/forest/Rock_3_A_Color1.obj','rock'),
    this.loadObj('./assets/kaykit/forest/Bush_1_A_Color1.obj','bush'),
-   this.loadObj('./assets/kaykit/forest/Grass_1_A_Color1.obj','grass')
-  ]).then(([tree1,tree2,tree4,rock,bush,grass])=>{
-   this.prototypes={tree:[tree1,tree2,tree4],rock,bush,grass};
+   this.loadObj('./assets/kaykit/forest/Bush_2_A_Color1.obj','bush'),
+   this.loadObj('./assets/kaykit/forest/Grass_1_A_Color1.obj','grass'),
+   this.loadObj('./assets/kaykit/forest/Grass_2_A_Color1.obj','grass')
+  ]).then(([tree1,tree2,tree4,bare1,rock1,rock2,rock3,bush1,bush2,grass1,grass2])=>{
+   this.prototypes={
+    tree:[tree1,tree2,tree4],
+    bareTree:[bare1],
+    rock:[rock1,rock2,rock3],
+    bush:[bush1,bush2],
+    grass:[grass1,grass2]
+   };
    return this.prototypes;
   });
   return this.loading;
@@ -107,18 +116,20 @@ export class EnvironmentPopulation {
  }
  populate(){
   this.root.clear();let placed=0;
-  for(let i=0;i<1040;i++){
+  for(let i=0;i<1100;i++){
    const a=this.rand(i*4)*Math.PI*2,r=13+Math.sqrt(this.rand(i*4+1))*116;
    const x=Math.cos(a)*r,z=Math.sin(a)*r,y=this.world.heightAt(x,z),s=this.slopeAt(x,z);
    if(y<.12||s>.78||Math.hypot(x,z)<12)continue;
    const forestBias=Math.max(0,1-r/125),roll=this.rand(i*4+2);let type;
-   if(roll<(.44+.18*forestBias)&&s<.48)type='tree';
-   else if(roll<.68)type='rock';
+   if(roll<(.44+.18*forestBias)&&s<.48){
+    type=this.rand(i*17+11)<.055?'bareTree':'tree';
+   }else if(roll<.68)type='rock';
    else if(roll<.86)type='bush';
    else type='grass';
    const o=this.clone(type,i*19+7);if(!o)continue;
    let scale=.78+this.rand(i*4+3)*.88;
    if(type==='tree')scale*=1.12;
+   else if(type==='bareTree')scale*=1.08;
    else if(type==='rock')scale*=2.15;
    else if(type==='bush')scale*=4.15;
    else scale*=2.15;
