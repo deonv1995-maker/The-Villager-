22bd4b4c197f4635338d14d741eb301ddd5c08dd
deonv1995-maker <deonv1995@gmail.com>
2026-08-25 21:05:46 +0200

    Load varied terrain formation 0.5.25

--- src/systems/WorldManager.js
+++ src/systems/WorldManager.js
@@ -1,6 +1,6 @@
 import { IslandTerrain } from './world/IslandTerrain.js?v=523';
 import { EnvironmentPopulation } from './world/EnvironmentPopulation.js?v=520';
-import { TerrainFeatures } from './world/TerrainFeatures.js?v=524';
+import { TerrainFeatures } from './world/TerrainFeatures.js?v=525';
 
 export class WorldManager {
  constructor(THREE, scene) {
