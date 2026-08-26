27f06758d1e1a6b5bd613f7d6eed34a719ff5c93
deonv1995-maker <deonv1995@gmail.com>
2026-08-26 04:01:45 +0200

    Load cliff seam backstop build

--- src/systems/WorldManager.js
+++ src/systems/WorldManager.js
@@ -1,6 +1,6 @@
 import { IslandTerrain } from './world/IslandTerrain.js?v=533';
 import { EnvironmentPopulation } from './world/EnvironmentPopulation.js?v=520';
-import { TerrainFeatures } from './world/TerrainFeatures.js?v=527';
+import { TerrainFeatures } from './world/TerrainFeatures.js?v=534';
 
 export class WorldManager {
  constructor(THREE, scene) {
